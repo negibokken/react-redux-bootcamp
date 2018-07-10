c6579c16cd3c780b3fdd8da38e358aeffa77131e
negibokken <negibokken@gmail.com>
2018-07-11 01:34:39 +0900

    improve ex1

--- ex1/src/App.js
+++ ex1/src/App.js
@@ -4,21 +4,40 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      num: 0
+      num10: 0,
+      num1: 0
     };
   }
   increment() {
-    const num = this.state.num + 1;
-    this.setState({ num: num });
+    let num1 = this.state.num1;
+    let num10 = this.state.num10;
+    num1 = num1 + 1;
+    if (num1 >= 10) {
+      num10 = num10 + 1;
+      num1 = 0;
+    }
+    this.setState({ num10: num10, num1: num1 });
   }
   decrement() {
-    const num = this.state.num - 1;
-    this.setState({ num: num });
+    let num1 = this.state.num1;
+    let num10 = this.state.num10;
+    num1 = num1 - 1;
+    if (num1 <= -1) {
+      if (num10 > 0) {
+        num10 = num10 - 1;
+        num1 = 9;
+      } else {
+        num1 = 0;
+      }
+    }
+    this.setState({ num10: num10, num1: num1 });
   }
   render() {
     return (
       <div>
-        <div>{this.state.num}</div>
+        <div>{this.state.num10}</div>
+        <div>{this.state.num1}</div>
+        <br />
         <button onClick={this.increment.bind(this)}>+</button>
         <button onClick={this.decrement.bind(this)}>-</button>
       </div>
