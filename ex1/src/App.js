e370dc21d6c037dad2698c7cd437bea8f86bf75e
negibokken <negibokken@gmail.com>
2018-07-10 23:35:24 +0900

    Fix ex1

--- ex1/src/App.js
+++ ex1/src/App.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      num: 0
+    };
+  }
+  increment() {
+    const num = this.state.num + 1;
+    this.setState({ num: num });
+  }
+  decrement() {
+    const num = this.state.num - 1;
+    this.setState({ num: num });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div>{this.state.num}</div>
+        <button onClick={this.increment.bind(this)}>+</button>
+        <button onClick={this.decrement.bind(this)}>-</button>
       </div>
     );
   }
